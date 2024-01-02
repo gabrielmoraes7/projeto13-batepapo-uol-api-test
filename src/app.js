0766f3f622c9c6d71b45f97e1a7e5dc8551a31ab
gabrielmoraes7 <gabriel.moclemente7@gmail.com>
2024-01-02 12:45:13 -0300

    base e inicialização correta agora+ adição do chalk

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 //importação das bibliotecas utilizadas
-import { MongoClient, MongoClient, ObjectId } from 'mongodb';
+import { mongoClient, MongoClient, ObjectId } from 'mongodb';
 import express, {json} from 'express';
-import chalk from 'chalk';
 import dayjs from 'dayjs';
+import chalk from 'chalk';
 import Joi from 'joi';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -12,6 +12,7 @@ dotenv.config();
 const MongoClient = new MongoClient(process.env.DATABASE_URL);
 const app = express();
 
+//formato dos objetos das mensagens a serem guardadas/exibidas
 const messageSchema = joi.object({
     to: joi.string().required(),
     text: joi.string().required(),
@@ -19,4 +20,22 @@ const messageSchema = joi.object({
 });
 
 start();
+async function start() {
+
+    app.use(cors());
+    app.use(json());
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Conexão feita na porta: ${process.env.PORT}`);
+    });
+
+    try {
+        await mongoClient.connect();
+        console.log('Conexão ao banco de dados feita com exito!!');
+    } catch (err) {
+        console.log(chalk.bold.red(err.message));
+    }
+}
+
+const db = mongoClient.db();
 
