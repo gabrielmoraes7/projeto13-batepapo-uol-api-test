b4bd4f883464bdf1b6b798350dbe670f6465471d
gabrielmoraes7 <gabriel.moclemente7@gmail.com>
2024-01-02 13:09:12 -0300

    rota POST /participants finalizada

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import chalk from 'chalk';
 import Joi from 'joi';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { stripHtml } from 'string-strip-html';
 
 //conexão local ao banco de dados Mongo pela variavel local no dotenv
 dotenv.config();
@@ -38,4 +39,60 @@ async function start() {
 }
 
 const db = mongoClient.db();
+
+
+// Inicio das requisições POST e GET
+app.post('/participants', async (req, res) => {
+
+    let { name } = req.body;
+
+    if(!isNaN(name))return res.sendStatus(422);
+
+    if (name) {
+        name = stripHtml(name.toString()).result.trim();
+    }
+
+    const userSchema = joi.object({
+        name: joi.any().required()
+    });
+ 
+    //valida a formatação dos dados do usuario
+    const validation = userSchema.validate(req.body, { abortEarly: false });
+
+    //detalha em caso de erros de validação 
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    if(name === "") return res.status(422).send('Por favor, preencha o nome!');
+
+    try {
+        const hasUser = await db.collection('participants').findOne({ name });
+        if (hasUser) {
+            console.log(`Usuario ${name} já está logado`);
+            return res.status(409).send('Usuario já existe!');
+        }
+        else {
+            await db.collection('participants').insertOne({
+                name,
+                lastStatus: Date.now()
+            });
+
+            await db.collection('messages').insertOne(
+                {
+                from: name,
+                to: 'Todos',
+                text: 'entra na sala...',
+                type: 'status',
+                time: dayjs().format('HH:mm:ss'),
+                }
+            );
+            return res.sendStatus(201);
+        }
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+    
+});  
 
